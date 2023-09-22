a376534b35f5dcf0b55d33a616e51e7f10b77212
Sameet <208490+sameet@users.noreply.github.com>
2023-09-22 16:59:21 -0400

    Adding place-holders for functionality. Adding packages that are going to be used later.

--- R/read_count_mat.R
+++ R/read_count_mat.R
@@ -1,6 +1,7 @@
 #' Read in the counts
 #'
 #' @param fn path to a file name typically a csv file.
+#' @param meta_df A meta-data frame.
 #'
 #' @return A matrix of counts of integers.
 #' @export
@@ -11,12 +12,23 @@
 #' fn <- file.path("tests", "test_data", "gene_count_matrix_fixed.csv")
 #' count_mat <- read_count_mat(fn)
 #' }
-read_count_mat <- function(fn) {
+read_count_mat <- function(fn, meta_df) {
   if(!fs::is_file(fn)) {
     stop("Need count file.")
   }
 
+  if(!is.data.frame(meta_df)) {
+    stop("Meta-data needs to be a data-frame")
+  }
+
   fn |>
     readr::read_delim(",") |>
-    as.matrix()
+    tidyr::pivot_longer(cols = !c("gene_id"), names_to = "sample", values = "counts") |>
+    dplyr::group_by(gene_id, sample) |>
+    dplyr::summarize(fixed_counts = sum(counts)) |>
+    tidyr::pivot_longer(id_cols = "gene_id", names_from = "sample", values_from = "fixed_counts",
+                        value_fill = 0) |>
+    as.matrix() -> count_mat
+  count_mat <- count_mat[, rownames(meta_df)]
+  count_mat
 }
