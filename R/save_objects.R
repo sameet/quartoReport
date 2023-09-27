8916dc9a505af647be8f792f9f26851140f55aad
Sameet <208490+sameet@users.noreply.github.com>
2023-09-27 14:51:48 -0400

    Basic pipeine now working.

--- R/save_objects.R
+++ R/save_objects.R
@@ -1,5 +1,21 @@
-save_objects <- function(obj) {
+#' Save computed DESEq2 objects.
+#'
+#' @param obj Object to be saved, either a DESEq2 experiment or vst data
+#' @param op_dir Directory where the files will be saved.
+#'
+#' @return no return value the objects are saved to the output directory
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' save_objects(dds, op_dir = ".")
+#' }
+save_objects <- function(obj, op_dir = NULL) {
+  if(is.null(op_dir)) {
+    op_dir = getwd()
+  }
   curr_date <- Sys.time() |>
     format("%Y-%M-%d_%H-%m")
-  saveRDS(obj, file = paste(class(obj)[1], "-", curr_date, ".RDS", sep = ""))
+  message("Saving the computed objects to: ", op_dir)
+  saveRDS(obj, file = file.path(op_dir, paste(class(obj)[1], "-", curr_date, ".RDS", sep = "")))
 }
