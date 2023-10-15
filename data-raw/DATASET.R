9ccd8073bec276f2a479fb476b6b16cc4f31d512
Sameet <208490+sameet@users.noreply.github.com>
2023-10-15 19:30:15 -0400

    Adding raw data to the package. This should make testing and other things realtively easier.

---
+++ data-raw/DATASET.R
@@ -0,0 +1,5 @@
+## code to prepare `DATASET` dataset goes here
+# WE only need some toy data to make sure that the part of this code works.
+# We decided to use the data from GEO for this purpose.
+# The data is simple and small enough and can potentially be bundled with the package.
+# Right now it is difficult to extract the meta-data from the existing data.
